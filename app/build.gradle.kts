2fe36cae5ecdea91d825d296d4185835ab63f717
rsaleemtma <rsaleem@tma.gr>
2026-08-20 21:42:18 +0500

    chore: Update Version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.rashidsaleem.notesapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "com.rashidsaleem.notesapp.HiltTestRunner"
         vectorDrawables {
